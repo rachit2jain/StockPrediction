fcea89b3122f4301063341eff67277903328ead8
rachit2jain <rachit2jain@gmail.com>
2016-10-01 19:22:55 +0800

    Update interEx.js

--- interEx.js
+++ interEx.js
@@ -1,150 +1,166 @@
 var _ = require('lodash');
 var http = require('http');
 var syncRequest = require('sync-request');
-
-callback = function(response){
-    // Continuously update stream with data
-    console.log("Test", response);
-}
-
+const PORT=$PORT; 
 const TEAM_UID="PkkYempGWJeQr3AFYzcOWA";
 
-function requestToApi(apiFunctions){
-    if (apiFunctions.apiCall === 'orders'){
-
-    	apiFunctions.orderTicket.team_uid = TEAM_UID;
-        apiFunctions.orderTicket.symbol = apiFunctions.symbol;
-
-        var request = syncRequest(
-        	'POST',
-        	'http://cis2016-'+apiFunctions.exchange+'.herokuapp.com/api/'+apiFunctions.apiCall,
-        	{
-        		'headers': {
-        			'Content-Type': 'application/json'
-        		},
-        		'json': apiFunctions.orderTicket
-        	}
-        );
-
-    	return JSON.parse(request.getBody());
-
-    } else {
-        if (apiFunctions.symbol) {
-            apiFunctions.apiCall = apiFunctions.apiCall+'/'+apiFunctions.symbol;
-        }
-
-        var request = syncRequest(
-        	'GET',
-        	'http://cis2016-'+apiFunctions.exchange+'.herokuapp.com/api/'+apiFunctions.apiCall
-        );
-
-        return JSON.parse(request.getBody());
-
-    }
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
 }
 
-while(true){
-var stockJSONex1 = requestToApi({
-	'apiCall':'market_data',
-	'symbol': '0386',
-	'exchange': 'exchange1'
-});
+//Create a server
+var server = http.createServer(handleRequest);
 
-var stockJSONex2 = requestToApi({
-	'apiCall':'market_data',
-	'symbol': '0386',
-	'exchange': 'exchange2'
-});
+// Connect to the db
+// MongoClient.connect("mongodb://localhost:27017/stockPredict", function(err, db) {
+// if(!err) {
+//     console.log("We are connected");
+// }
+// });
 
-var stockJSONex3 = requestToApi({
-	'apiCall':'market_data',
-	'symbol': '0386',
-	'exchange': 'exchange3'
-});
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
 
 
-var stockData = [stockJSONex1, stockJSONex2, stockJSONex3];
+	function requestToApi(apiFunctions){
+	    if (apiFunctions.apiCall === 'orders'){
 
-//console.log("Session: %j", _.toPairs(stockJSONex1.buy));
-//console.log(stockData);
+			apiFunctions.orderTicket.team_uid = TEAM_UID;
+			apiFunctions.orderTicket.symbol = apiFunctions.symbol;
 
-var minPrice = 10000000;
-var exchangeOfMin = 4;
+			var request = syncRequest(
+				'POST',
+				'http://cis2016-'+apiFunctions.exchange+'.herokuapp.com/api/'+apiFunctions.apiCall,
+				{
+					'headers': {
+						'Content-Type': 'application/json'
+					},
+					'json': apiFunctions.orderTicket
+				}
+			);
 
-var i = 1;
-_.forEach(stockData, function(value) {
-	//console.log("Sell: %j\n", value.sell);
-	var min = 10000000;
-	_.forEach(value.sell, function(key, val){
-		if(val < min){
-			min = val;
+			return JSON.parse(request.getBody());
+
+		    } else {
+			if (apiFunctions.symbol) {
+			    apiFunctions.apiCall = apiFunctions.apiCall+'/'+apiFunctions.symbol;
+			}
+
+			var request = syncRequest(
+				'GET',
+				'http://cis2016-'+apiFunctions.exchange+'.herokuapp.com/api/'+apiFunctions.apiCall
+			);
+
+			return JSON.parse(request.getBody());
+
+		    }
 		}
-	});
-	
-	if(min < minPrice){
-		minPrice = min;
-		exchangeOfMin = i;
-	}
-	i++;
-});
 
-// console.log(minPrice);
-// console.log(stockData[exchangeOfMin-1].sell[minPrice]);
-// console.log(exchangeOfMin);
+		while(true){
+		var stockJSONex1 = requestToApi({
+			'apiCall':'market_data',
+			'symbol': '0386',
+			'exchange': 'exchange1'
+		});
 
-var maxPrice = -10;
-var exchangeOfMax = 4;
-var i = 1;
+		var stockJSONex2 = requestToApi({
+			'apiCall':'market_data',
+			'symbol': '0386',
+			'exchange': 'exchange2'
+		});
 
-_.forEach(stockData, function(value) {
-	//console.log("Buy: %j\n", value.buy);
-	var max = -10;
-	_.forEach(value.buy, function(key, val){
-		if(val > max){
-			max = val;
+		var stockJSONex3 = requestToApi({
+			'apiCall':'market_data',
+			'symbol': '0386',
+			'exchange': 'exchange3'
+		});
+
+
+		var stockData = [stockJSONex1, stockJSONex2, stockJSONex3];
+
+		//console.log("Session: %j", _.toPairs(stockJSONex1.buy));
+		//console.log(stockData);
+
+		var minPrice = 10000000;
+		var exchangeOfMin = 4;
+
+		var i = 1;
+		_.forEach(stockData, function(value) {
+			//console.log("Sell: %j\n", value.sell);
+			var min = 10000000;
+			_.forEach(value.sell, function(key, val){
+				if(val < min){
+					min = val;
+				}
+			});
+
+			if(min < minPrice){
+				minPrice = min;
+				exchangeOfMin = i;
+			}
+			i++;
+		});
+
+		// console.log(minPrice);
+		// console.log(stockData[exchangeOfMin-1].sell[minPrice]);
+		// console.log(exchangeOfMin);
+
+		var maxPrice = -10;
+		var exchangeOfMax = 4;
+		var i = 1;
+
+		_.forEach(stockData, function(value) {
+			//console.log("Buy: %j\n", value.buy);
+			var max = -10;
+			_.forEach(value.buy, function(key, val){
+				if(val > max){
+					max = val;
+				}
+			});
+
+			if(max > maxPrice){
+				maxPrice = max;
+				exchangeOfMax = i;
+			}
+			i++;
+		});
+
+		// console.log(maxPrice);
+		// console.log(stockData[exchangeOfMax-1].buy[maxPrice]);
+		// console.log(exchangeOfMax);
+
+		if( stockData[exchangeOfMax-1].buy[maxPrice] > stockData[exchangeOfMin-1].sell[minPrice]){
+			var quantity = stockData[exchangeOfMin-1].sell[minPrice];
+		}else{
+			var quantity = stockData[exchangeOfMax-1].buy[maxPrice];
 		}
-	});
-	
-	if(max > maxPrice){
-		maxPrice = max;
-		exchangeOfMax = i;
-	}
-	i++;
+
+		console.log("Buy "+quantity+ " from exchange "+ exchangeOfMin +" and sell to " + exchangeOfMax);
+		console.log("Buy "+minPrice+ " from exchange "+ exchangeOfMin +" and sell at " + maxPrice);
+
+		// var output = requestToApi({
+		//         'apiCall':'orders',
+		//         'symbol': '0005',
+		//         'exchange': 'exchange'+exchangeOfMin,
+		//         'orderTicket': {"side": "buy",
+		//                         "qty":quantity,
+		//                         "order_type":"market"}
+		//     });
+
+		//     console.log(output);
+
+		// 	var output2 = requestToApi({
+		//         'apiCall':'orders',
+		//         'symbol': '0005',
+		//         'exchange': 'exchange'+exchangeOfMax,
+		//         'orderTicket': {"side": "sell",
+		//                         "qty":quantity,
+		//                         "order_type":"market"}
+		//     });
+
+		    // console.log(output2);
+		}
 });
-
-// console.log(maxPrice);
-// console.log(stockData[exchangeOfMax-1].buy[maxPrice]);
-// console.log(exchangeOfMax);
-
-if( stockData[exchangeOfMax-1].buy[maxPrice] > stockData[exchangeOfMin-1].sell[minPrice]){
-	var quantity = stockData[exchangeOfMin-1].sell[minPrice];
-}else{
-	var quantity = stockData[exchangeOfMax-1].buy[maxPrice];
-}
-
-console.log("Buy "+quantity+ " from exchange "+ exchangeOfMin +" and sell to " + exchangeOfMax);
-console.log("Buy "+minPrice+ " from exchange "+ exchangeOfMin +" and sell at " + maxPrice);
-
-// var output = requestToApi({
-//         'apiCall':'orders',
-//         'symbol': '0005',
-//         'exchange': 'exchange'+exchangeOfMin,
-//         'orderTicket': {"side": "buy",
-//                         "qty":quantity,
-//                         "order_type":"market"}
-//     });
-
-//     console.log(output);
-	
-// 	var output2 = requestToApi({
-//         'apiCall':'orders',
-//         'symbol': '0005',
-//         'exchange': 'exchange'+exchangeOfMax,
-//         'orderTicket': {"side": "sell",
-//                         "qty":quantity,
-//                         "order_type":"market"}
-//     });
-
-    // console.log(output2);
-}
 
