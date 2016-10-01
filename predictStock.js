8435f3ae7cdc8a8e45426ed5b29503dcbd5ad21a
Aman Gupta <amangupta96@outlook.com>
2016-10-01 16:00:47 +0800

    Used Promise to fix sync shit

--- predictStock.js
+++ predictStock.js
@@ -1,15 +1,16 @@
 //Lets require/import the HTTP module
 var http = require('http');
-var unirest =require('unirest')
+var unirest = require('unirest');
+var _ = require('lodash');
 
 // var MongoClient = require('mongodb').MongoClient;
 
 //Lets define a port we want to listen to
-const PORT=8080; 
-const TEAM_UID="PkkYempGWJeQr3AFYzcOWA";
+const PORT = 8080;
+const TEAM_UID = "PkkYempGWJeQr3AFYzcOWA";
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
+function handleRequest(request, response) {
     response.end('It Works!! Path Hit: ' + request.url);
 }
 
@@ -24,71 +25,90 @@ var server = http.createServer(handleRequest);
 // });
 
 //Lets start our server
-server.listen(PORT, function(){
+server.listen(PORT, function () {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
     // requestToTeam();
- 	requestToApi({
-        'apiCall':'market_data',
+
+    var exchange = "exchange1";
+    options = {
+        'apiCall': 'market_data',
         'symbol': '0001',
-        'exchange': 'exchange1',
-        'orderTicket': {"side": "buy",
-                        "qty":1,
-                        "order_type":"market"}
-    });
+        'exchange': exchange,
+        'orderTicket': {
+            "side": "buy",
+            "qty": 1,
+            "order_type": "market"
+        }
+    };
+    var test = requestToApi(options);
+    test.then(function (val) {
+        console.log(val);
+    }).catch(
+        // Log the rejection reason
+        function (reason) {
+            console.log('Handle rejected promise (' + reason + ') here.');
+        });
 });
 
-callback = function(response){
+callback = function (response) {
     // Continuously update stream with data
     console.log("Test", response);
 }
 
 function requestToTeam() {
-    var request = unirest.get('http://cis2016-teamtracker.herokuapp.com/api/teams/'+TEAM_UID);
-    request.headers({'Content-Type': 'application/json'});
-	request.end(function (response) {
-		// return response;
-	  callback(response.body);
-	  // return response;
-	});
+    var request = unirest.get('http://cis2016-teamtracker.herokuapp.com/api/teams/' + TEAM_UID);
+    request.headers({ 'Content-Type': 'application/json' });
+    request.end(function (response) {
+        // return response;
+        callback(response.body);
+        // return response;
+    });
 }
 
 
 /**
- * apiFunctions:
- * exchange: Exchange 1/2/3
- * apiCall: market_data,market_data/$symbol, orders
- * symbol
- * orderTicket: {"side": "buy",
-                "qty":1,
-                "order_type":"market"}
- * 
- */
+* apiFunctions:
+* exchange: Exchange 1/2/3
+* apiCall: market_data,market_data/$symbol, orders
+* symbol
+* orderTicket: {"side": "buy",
+               "qty":1,
+               "order_type":"market"}
+* 
+*/
 
 
-function requestToApi(apiFunctions){
-    if (apiFunctions.apiCall === 'orders'){
-    	apiFunctions.orderTicket.team_uid = TEAM_UID;
+function requestToApi(apiFunctions) {
+    if (apiFunctions.apiCall === 'orders') {
+        apiFunctions.orderTicket.team_uid = TEAM_UID;
         apiFunctions.orderTicket.symbol = apiFunctions.symbol;
-    	var request = unirest.post('http://cis2016-'+apiFunctions.exchange+'.herokuapp.com/api/'+apiFunctions.apiCall);
-    	request.headers({'Content-Type': 'application/json'});
-    	request.send(apiFunctions.orderTicket);
-    	request.end(function (response) {
-    		callback(response.body);
-		  // console.log(response.body);
-		  // console.log(request);
-		});
+        var request = unirest.post('http://cis2016-' + apiFunctions.exchange + '.herokuapp.com/api/' + apiFunctions.apiCall);
+        request.headers({ 'Content-Type': 'application/json' });
+        request.send(apiFunctions.orderTicket);
+        return new Promise(
+            function (resolve, reject) {
+                request.end(function (response) {
+                    resolve(response.body);
+                })
+            }
+        );
 
     }
-    else{
+    else {
         if (apiFunctions.symbol)
-            apiFunctions.apiCall = apiFunctions.apiCall+'/'+apiFunctions.symbol;
-        var request = unirest.get('http://cis2016-'+apiFunctions.exchange+'.herokuapp.com/api/'+apiFunctions.apiCall);
-	    request.headers({'Content-Type': 'application/json'});
-    	request.end(function (response) {
-    		// return response;
-		  callback(response.body);
-		  // return response;
-		});
+            apiFunctions.apiCall = apiFunctions.apiCall + '/' + apiFunctions.symbol;
+        var request = unirest.get('http://cis2016-' + apiFunctions.exchange + '.herokuapp.com/api/' + apiFunctions.apiCall);
+        request.headers({ 'Content-Type': 'application/json' });
+        return new Promise(
+            // The resolver function is called with the ability to resolve or
+            // reject the promise
+            function (resolve, reject) {
+                // This is only an example to create asynchronism
+                request.end(function (response) {
+                    resolve(response.body);
+                })
+            }
+        );
     }
 }
